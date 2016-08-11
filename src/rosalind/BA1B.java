fb39d13956d0516ac12237779825855e4cdb3da8
Sergei Malgin <Sergei_Malgin@epam.com>
2016-08-11 09:53:20 +0300

    Rosalind DNA

---
+++ src/rosalind/BA1B.java
@@ -0,0 +1,32 @@
+package rosalind;
+
+/*
+Frequent Words Problem
+
+Find the most frequent k-mers in a string.
+
+We say that Pattern is a most frequent k-mer in Text
+if it maximizes Count(Text, Pattern) among all k-mers.
+For example, "ACTAT" is a most frequent 5-mer in
+"ACAACTATGCATCACTATCGGGAACTATCCT",
+and "ATA" is a most frequent 3-mer of "CGATATATCCATAG".
+
+Given: A DNA string Text and an integer k.
+
+Return: All most frequent k-mers in Text (in any order).
+*/
+
+import java.util.List;
+
+public class BA1B {
+
+    /**
+     * Find the most frequent k-mers in a DNA string.
+     * @param dna   DNA string
+     * @param k     k-mer length
+     * @return      All most frequent k-mers in Text
+     */
+    public static List<String> process(String dna, int k) {
+
+    }
+}
