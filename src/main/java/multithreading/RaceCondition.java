b2ba4a5d336d09de5412d2ed9c6f50ac26d23ca9
Sergei Malgin <Sergei_Malgin@epam.com>
2017-05-20 11:20:52 +0300

    CorrectCounter added to RaceCondition

--- src/main/java/multithreading/RaceCondition.java
+++ src/main/java/multithreading/RaceCondition.java
@@ -1,12 +1,14 @@
 package multithreading;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class RaceCondition {
 
     private static int count = 0;
+    private static AtomicInteger atomicCount;
     private static int AMOUNT = 1_000_000;
 
-    private static class Counter implements Runnable {
-
+    private static class BuggyCounter implements Runnable {
         @Override
         public void run() {
             for (int i = 0; i < AMOUNT; i++) {
@@ -15,19 +17,31 @@ public class RaceCondition {
         }
     }
 
+    private static class CorrectCounter implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < AMOUNT; i++) {
+                atomicCount.incrementAndGet();
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        Runnable counter = new BuggyCounter();
+        fireCounter(counter);
+        System.out.println("Using BuggyCounter = " + count);
+        atomicCount = new AtomicInteger();
+        counter = new CorrectCounter();
+        fireCounter(counter);
+        System.out.println("Using CorrectCounter = " + atomicCount.get());
+    }
 
-        Counter counter = new Counter();
-
+    private static void fireCounter(Runnable counter) throws InterruptedException {
         Thread thread1 = new Thread(counter);
         Thread thread2 = new Thread(counter);
-
         thread1.start();
         thread2.start();
-
         thread1.join();
         thread2.join();
-
-        System.out.println("Count = " + count);
     }
 }
