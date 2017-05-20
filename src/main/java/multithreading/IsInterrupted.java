f04e10cfead24f1787fa20ab7d169a2cae01032e
Sergei Malgin <Sergei_Malgin@epam.com>
2017-05-20 09:42:17 +0300

    Ticker implements Runnable

--- src/main/java/multithreading/IsInterrupted.java
+++ src/main/java/multithreading/IsInterrupted.java
@@ -4,13 +4,13 @@ import util.Utils;
 
 public class IsInterrupted {
 
-    static private class Ticker extends Thread {
+    static private class Ticker implements Runnable {
         @Override
         public void run() {
             String name = Thread.currentThread().getName();
             System.out.println(name + " is started");
             int i = 0;
-            while (!this.isInterrupted()) {
+            while (!Thread.currentThread().isInterrupted()) {
                 i++;
                 if(i % 200_000_000 == 0) {
                     System.out.println(i);
@@ -22,10 +22,10 @@ public class IsInterrupted {
 
     public static void main(String[] args) {
 
-        Ticker ticker = new Ticker();
+        Thread ticker = new Thread(new Ticker());
         ticker.start();
 
-        Utils.delay(5_000);
+        Utils.delay(3_000);
 
         ticker.interrupt();
     }
