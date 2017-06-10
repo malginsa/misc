53aba8ba7c748eff6f63ad1e0f158b0b8a33051b
Sergei Malgin <Sergei_Malgin@epam.com>
2017-06-10 21:36:40 +0300

    UnmodifiableCollection is now in Kotlin

---
+++ src/main/java/misc/UnmodifiableCollection.kt
@@ -0,0 +1,21 @@
+package misc
+
+import java.util.*
+
+object UnmodifiableCollection {
+
+    @JvmStatic fun main(args: Array<String>) {
+
+        val list = ArrayList<Int>()
+
+        list.add(1)
+        list.add(2)
+        list.add(3)
+
+        val unmodifiableCollection = Collections.unmodifiableCollection(list)
+
+        //        unmodifiableCollection.add(4); // throws UnsupportedOperationException
+
+    }
+
+}
