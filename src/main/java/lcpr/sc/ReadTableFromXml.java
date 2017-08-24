c2fa6f8b76f529f534650a07268f51fc07a2b23f
Sergei_Malgin <malginsa@gmail.com>
2017-08-24 20:48:06 +0300

    readByJdom added

---
+++ src/main/java/lcpr/sc/ReadTableFromXml.java
@@ -0,0 +1,30 @@
+package lcpr.sc;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadTableFromXml {
+
+    public static void readByJdom(String[] args) {
+
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File("src\\main\\resources\\SLPCT2HX.xml");
+
+        Document document = null;
+        try {
+            document = builder.build(xmlFile);
+        } catch (JDOMException | IOException e) {
+//            LOG.ERR
+            e.printStackTrace();
+        }
+        Element root = document.getRootElement();
+        List rules = root.getChildren("rule");
+        System.out.println(rules.size());
+    }
+}
