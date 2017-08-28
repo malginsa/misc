a2686e0dfa30381d8b72a04b42bb27092a04ff54
Sergei_Malgin <malginsa@gmail.com>
2017-08-28 12:27:25 +0300

    ReadTableFromXml first version

--- src/main/java/lcpr/sc/ReadTableFromXml.java
+++ src/main/java/lcpr/sc/ReadTableFromXml.java
@@ -4,6 +4,11 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,10 +16,45 @@ import java.util.List;
 
 public class ReadTableFromXml {
 
-    public static void readByJdom(String[] args) {
+    private static final String FILE_NAME = "src\\main\\resources\\SLPCT2HX.xml";
+
+
+    private static class ReaderBySax extends DefaultHandler{
+
+        @Override
+        public void startElement (String uri, String localName, String qName, Attributes attributes) {
+            System.out.print("<" + qName + ">");
+        }
+
+        @Override
+        public void endElement (String uri, String localName, String qName) {
+            System.out.print("</" + qName + ">\n");
+        }
+
+        @Override
+        public void characters (char ch[], int start, int length) {
+//            for (int i = start; i < (start + length); i++) {
+//                System.out.print(ch[i]);
+//            }
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        readBySax();
+    }
+
+    public static void readBySax() throws Exception {
+        XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+        xmlReader.setContentHandler(new ReaderBySax());
+        xmlReader.parse(FILE_NAME);
+    }
+
+    public static void readByJdom() throws Exception {
 
         SAXBuilder builder = new SAXBuilder();
-        File xmlFile = new File("src\\main\\resources\\SLPCT2HX.xml");
+        File xmlFile = new File(FILE_NAME);
 
         Document document = null;
         try {
@@ -24,7 +64,11 @@ public class ReadTableFromXml {
             e.printStackTrace();
         }
         Element root = document.getRootElement();
+        if (null == root) {
+            throw new Exception("Error root element");
+        }
         List rules = root.getChildren("rule");
         System.out.println(rules.size());
     }
+
 }
--- src/main/java/lcpr/sc/TableToXmlConverter.java
+++ src/main/java/lcpr/sc/TableToXmlConverter.java
@@ -1,9 +1,15 @@
 package lcpr.sc;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,14 +26,21 @@ public class TableToXmlConverter extends DefaultHandler{
         InputStreamReader inputStreamReader = null;
         BufferedReader br = null;
 
+        Path input = Paths.get(INPUT_FILE_NAME);
         Path output = Paths.get(INPUT_FILE_NAME + ".xml");
 
         try {
-            inputStream = new FileInputStream(new File(INPUT_FILE_NAME)) {};
-            inputStreamReader = new InputStreamReader(inputStream);
-            br = new BufferedReader(inputStreamReader);
+//            inputStream = new FileInputStream(new File(INPUT_FILE_NAME));
+//            inputStreamReader = new InputStreamReader(inputStream);
+//            br = new BufferedReader(inputStreamReader);
+
+//            FileReader fileReader = new FileReader(INPUT_FILE_NAME);
+//            br = new BufferedReader(fileReader);
+
+            byte[] bytes = Files.readAllBytes(input);
+
             bw = Files.newBufferedWriter(output, Charset.defaultCharset());
-            bw.write("<?xml version=\"1.0\"?>\n");
+            bw.write("<?xml version=\"1.0\" encoding=\"latin-1\"?>\n");
             bw.write("<root>\n");
             bw.write("<description>\n");
             bw.write("\tThis table is used by the WFPCTRANS program to translate percent\n" +
@@ -48,8 +61,8 @@ public class TableToXmlConverter extends DefaultHandler{
             try {
                 bw.close();
                 br.close();
-                inputStreamReader.close();
-                inputStream.close();
+//                inputStreamReader.close();
+//                inputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -62,6 +75,7 @@ public class TableToXmlConverter extends DefaultHandler{
         if ((null == line) || line.isEmpty() || (line.charAt(0) != '%') || line.length() < 9) {
             return;
         }
+        byte[] bytes = line.getBytes();
         String percentAsString = line.substring(1, 3);
         Integer percent = Integer.decode("0x" + percentAsString);
         String value = line.substring(8, line.length());
@@ -79,12 +93,21 @@ public class TableToXmlConverter extends DefaultHandler{
                     bw.write(from);
                 bw.write("</from>\n");
                 bw.write("\t\t<to>");
-                    bw.write(to);
+                    String legalized = legalizeToXml(to);
+                    bw.write(legalized); // &#x91;
                 bw.write("</to>\n");
             bw.write("\t</rule>\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String legalizeToXml(String input) {
+        StringBuilder result = new StringBuilder();
+        byte[] bytes = input.getBytes();
+        return input;
+//        '\x09', '\x0A\', \'\x0D\', \'\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF
+
     }
 
 }
