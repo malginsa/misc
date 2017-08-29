e8635f21387f02bc268f43592c44f82b745a06a4
Sergei_Malgin <malginsa@gmail.com>
2017-08-29 14:43:09 +0300

    TableXmlReader except %00

--- src/main/java/lcpr/sc/TableXmlReader.java
+++ src/main/java/lcpr/sc/TableXmlReader.java
@@ -1,32 +1,21 @@
 package lcpr.sc;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
 import org.xml.sax.Attributes;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 public class TableXmlReader {
 
     private static final String FILE_NAME = "src/main/resources/SLPCT2HX.xml";
     private static final String[][] convertionTable = new String[256][2];
 
-
     private static class ReaderBySax extends DefaultHandler{
 
         private boolean inTo = false;
         private boolean inFrom = false;
-        private int index = 0;
-        private String to = null;
-        private String from = null;
         private String accumulator = null;
+        private int index = -1;
 
         @Override
         public void startElement (String uri, String localName, String qName, Attributes attributes) {
@@ -45,7 +34,7 @@ public class TableXmlReader {
             }
             if ("to".equalsIgnoreCase(qName)) {
                 if (null != accumulator) {
-                    System.out.print("|" + accumulator + "|");
+                    convertionTable[index][1] = accumulator;
                     accumulator = null;
                 }
                 inTo = false;
@@ -68,27 +57,19 @@ public class TableXmlReader {
             }
             if (inFrom) {
                 index++;
-                this.from = value;
-                System.out.print("\n|" + this.from + "|\t");
+                convertionTable[index][0] = value;
+                convertionTable[index][1] = "";
                 return;
             }
             if (inTo) {
-                to = value;
-                System.out.print("|" + to + "|");
+                convertionTable[index][1] = value;
                 return;
             }
         }
-
     }
-
 
     public static void main(String[] args) throws Exception {
         readBySax();
-        for (int i = 0; i < convertionTable.length; i++) {
-            System.out.println("|"+convertionTable[i][0] + "|\t|" + convertionTable[i][1] + "|");
-        }
-        System.out.println("|" + convertionTable[12][0] + "|");
-        System.out.println("|" + convertionTable[12][1] + "|");
     }
 
     public static void readBySax() throws Exception {
@@ -96,25 +77,4 @@ public class TableXmlReader {
         xmlReader.setContentHandler(new ReaderBySax());
         xmlReader.parse(FILE_NAME);
     }
-
-    public static void readByJdom() throws Exception {
-
-        SAXBuilder builder = new SAXBuilder();
-        File xmlFile = new File(FILE_NAME);
-
-        Document document = null;
-        try {
-            document = builder.build(xmlFile);
-        } catch (JDOMException | IOException e) {
-//            LOG.ERR
-            e.printStackTrace();
-        }
-        Element root = document.getRootElement();
-        if (null == root) {
-            throw new Exception("Error root element");
-        }
-        List rules = root.getChildren("rule");
-        System.out.println(rules.size());
-    }
-
 }
