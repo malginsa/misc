bd760dcf15fc03ca6533111d08e2c8a0aea4ec09
malginsa <malginsa@gmail.com>
2017-09-01 11:27:34 +0300

    JaxbParser added

---
+++ src/main/java/xml/jaxb/parser/impl/JaxbParser.java
@@ -0,0 +1,27 @@
+package xml.jaxb.parser.impl;
+
+import xml.jaxb.parser.MyParser;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class JaxbParser implements MyParser {
+
+    @Override
+    public Object getObject(File file, Class c) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(c);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Object object = unmarshaller.unmarshal(file);
+        return object;
+    }
+
+    @Override
+    public void saveObject(File file, Object o) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(o.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.marshal(o, file);
+    }
+}
