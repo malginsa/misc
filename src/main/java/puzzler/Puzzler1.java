0a0fc17cbe056f3576409a34c4ef8edba132b5a9
malginsa <malginsa@gmail.com>
2019-01-16 14:40:42 +0300

    Puzzler1 added

---
+++ src/main/java/puzzler/Puzzler1.java
@@ -0,0 +1,13 @@
+package puzzler;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Puzzler1
+{
+    public static void main(String[] args)
+    {
+        List list = Arrays.asList(1, 2, 3, 4);
+        list.remove(0);
+    }
+}
